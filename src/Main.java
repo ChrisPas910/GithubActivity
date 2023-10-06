27ddf652a32faba703999189c6b6463f38b856ca
BROWNAM20 <brownAM20@gcc.edu>
2023-10-06 13:35:29 -0400

    Changed "world" to a scanned name given by the user

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Please state your name: ");
+        String name = scn.next();
+
+
+        System.out.println("Hello, " + name + "!");
     }
 }
