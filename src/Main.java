fd10a8761e9e462ac24db0723b4281464db9809b
BROWNAM20 <brownAM20@gcc.edu>
2023-10-09 13:36:00 -0400

    Extra stuff

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Please state your name: ");
+        System.out.println("Please state your name: " + "New stuff here.");
         String name = scn.next();
 
 
